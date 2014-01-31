c20299cacbbae2bead2063f1b7448c528b20facf
Sam Blackman <sblackman@gmail.com>
2014-01-31 16:33:18 -0600

    corrected deck_id assignment

--- app/controllers/stats.rb
+++ app/controllers/stats.rb
@@ -1,6 +1,6 @@
 get '/stats' do
   round = Round.find_by(id: session[:round_id])
-  @deck_id = round.cards.deck
+  @deck_id = round.cards.deck.id
   @correct = round.guesses.where(correctness = 1).count
   @incorrect = round.guesses.where(correctness = -1).count
   @skipped = round.guesses.where(correctness = 0).count
