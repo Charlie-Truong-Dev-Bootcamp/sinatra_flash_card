86a412b5573e40cbc173f551dbfd2d1642d10d90
gnitnuj <junting.lu@gmail.com>
2014-01-31 15:43:47 -0600

    Evicted unnecessary methods.

--- app/helpers/magic.rb
+++ app/helpers/magic.rb
@@ -4,16 +4,4 @@ helpers do
     Card.find(session[:cards][session[:num]])
   end
 
-  def correct_answer
-    "Correct!"
-  end
-
-  def skipped_answer
-    "Hmmm .... you're being quite lazy you skipped!"
-  end
-
-  def wrong_answer
-    "Whoops, not quite right! The answer you seek is #{find_a_card.answer}."
-  end
-
 end
