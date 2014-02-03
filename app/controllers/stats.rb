4a74ff8ca759148a54dd961bb2c9b86ac27931ae
Sam Blackman <sblackman@gmail.com>
2014-02-02 22:39:15 -0600

    added db calls to get global stats for a given deck

--- app/controllers/stats.rb
+++ app/controllers/stats.rb
@@ -1,9 +1,18 @@
+require 'pry'
+
 get '/stats' do
   round = Round.find(session[:round_id])
+
   @deck_id = round.cards.first.deck.id
   @correct = round.guesses.where(correctness: 1).count
   @incorrect = round.guesses.where(correctness: -1).count
   @skipped = round.guesses.where(correctness: 0).count
   @percent = (@correct / (@correct + @incorrect + @skipped).to_f ) * 100.0
+
+  @global_correct = Card.includes(:guesses).where(deck_id: @deck_id, "guesses.correctness" => 1) 
+  @global_incorrect = Card.includes(:guesses).where(deck_id: @deck_id, "guesses.correctness" => -1) 
+  @global_skipped = Card.includes(:guesses).where(deck_id: @deck_id, "guesses.correctness" => 0)
+  @global_percent = (@global_correct / (@global_correct + @global_incorrect + @global_skipped).to_f ) * 100.0
+  
   erb :stats
 end
